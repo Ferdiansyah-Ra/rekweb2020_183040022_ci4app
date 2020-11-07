b85d0b6352edab3805c44141a0195c36e8c14a2d
Ferdiansyah-Ra <71427560+Ferdiansyah-Ra@users.noreply.github.com>
2020-11-07 08:22:32 +0700

    update

--- app/Controllers/Komik.php
+++ app/Controllers/Komik.php
@@ -31,6 +31,57 @@ class Komik extends BaseController
             'title' => 'Detail Komik',
             'komik' => $this->komikModel->getKomik($slug)
         ];
+
+        // jika komik tiak ada ditabel
+        if (empty($data['komik'])) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException('Judul komik ' . $slug . ' tidak ditemukan.');
+        }
+
+
         return view('komik/detail', $data);
+    }
+
+    public function create()
+    {
+
+        // session();
+        $data = [
+            'title' => 'FormTambah Data Komik',
+            'validation' => \Config\Services::validation()
+        ];
+
+        return view('komik/create', $data);
+    }
+
+    public function save()
+    {
+
+        //validasi input
+        if (!$this->validate([
+            'judul' => [
+                'rules' => 'required|is_unique[komik.judul]',
+                'errors' => [
+                    'required' => '{field} komik harus diisi.',
+                    'is_unique' => '{field} komik sudah terdaftar'
+                ]
+            ]
+        ])) {
+            $validation = \Config\Services::validation();
+            return redirect()->to('/Komik/create')->withInput()->with('validation', $validation);
+        }
+
+
+        $slug = url_title($this->request->getVar('judul'), '-', true);
+        $this->komikModel->save([
+            'judul' => $this->request->getVar('judul'),
+            'slug' => $slug,
+            'penulis' => $this->request->getVar('penulis'),
+            'penerbit' => $this->request->getVar('penerbit'),
+            'sampul' => $this->request->getVar('sampul')
+        ]);
+
+        session()->setFlashdata('pesan', 'Data berhasil ditambahkan.');
+
+        return redirect()->to('/komik');
     }
 }
--- app/Views/komik/detail.php
+++ app/Views/komik/detail.php
@@ -13,8 +13,8 @@
                     <div class="col-md-8">
                         <div class="card-body">
                             <h5 class="card-title"><?= $komik['judul']; ?></h5>
-                            <p class="card-text"><b>Penulis : </b><?= $komik['penulis']; ?></p>
-                            <p class="card-text"><small class="text-muted"><b>Penerbit : </b><?= $komik['penerbit']; ?></small></p>
+                            <p class="card-text"><b>Penulis : </b> <?= $komik['penulis']; ?></p>
+                            <p class="card-text"><small class="text-muted"><b>Penerbit : </b> <?= $komik['penerbit']; ?></small></p>
 
                             <a href="" class="btn btn-warning">Edit</a>
                             <a href="" class="btn btn-danger"> Delete</a>
